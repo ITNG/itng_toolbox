190684d22ea97a910a9552643255287512739718
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2020-03-14 15:16:23 +0330

    make_grid added to drawing.

--- itng/doc/source/code.rst
+++ itng/doc/source/code.rst
@@ -19,3 +19,6 @@ Auto Generated Documentation
 
 .. automodule:: signal_processing
     :members:
+
+.. automodule:: drawing
+    :members:
--- itng/drawing.py
+++ itng/drawing.py
@@ -7,6 +7,8 @@ from copy import copy
 import networkx as nx
 from random import shuffle
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 class Drawing(object):
@@ -62,3 +64,43 @@ class Drawing(object):
             pl.savefig(fileName)
             pl.close()
 # ------------------------------------------------------------------#
+
+
+def make_grid(nrows,
+              ncols,
+              left=0.05,
+              right=0.95,
+              bottom=0.05,
+              top=0.95,
+              hspace=0.2,
+              wspace=0.2):
+    """
+    make grided figure
+
+    :param nrows: [int] number of rows in figure
+    :param ncols: [int] number of columns in figure
+    :param left, right, top, bottom: float, optional. Extent of the subplots as a fraction of figure width or height. Left cannot be larger than right, and bottom cannot be larger than top.
+    :param hspace: The amount of height reserved for space between subplots, expressed as a fraction of the average axis height.
+    :param wspace: The amount of width reserved for space between subplots, expressed as a fraction of the average axis width.
+    :return: axes of gridded figure
+
+    >>> import pylab as pl
+    >>> fig = pl.figure(figsize=(20, 15))
+    >>> ax = make_grid(2, 3, 0.1, 0.95, 0.1, 0.95, 0.2, 0.2)
+    >>> ax[0][0].plot(range(10), marker="o")
+    >>> pl.show()
+
+    """
+    
+    gs = GridSpec(nrows, ncols)
+    gs.update(left=left, right=right,
+              hspace=hspace, wspace=wspace,
+              bottom=bottom, top=top)
+    ax = []
+    for i in range(nrows):
+        ax_row = []
+        for j in range(ncols):
+            ax_row.append(pl.subplot(gs[i, j]))
+        ax.append(ax_row)
+    return ax
+# ---------------------------------------------------------------------- #
